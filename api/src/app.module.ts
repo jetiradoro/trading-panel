7b9e1843126b49d444a6a01906adb9f2d44b5b33
Jesús.T <jesus@jetiradoro.com>
2025-08-08 22:08:35 +0200

    routing logs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { LogsMiddleware } from './logs/logs.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogsMiddleware).forRoutes('*');
+  }
+}
