439af89259ad3f7c85ebd6a0d05a409d0f9a4ab5
Jesús.T <jesus@jetiradoro.com>
2026-01-20 21:53:21 +0100

    init container arreglado

--- docker/trading/api/init_container.sh
+++ docker/trading/api/init_container.sh
@@ -7,6 +7,8 @@ set -e
 # Aquí puedes agregar los comandos de inicialización necesarios
 echo "Inicializando el contenedor trading-api..."
 
+#tail -f /dev/null
+
 
 if [ ! -d "/app/node_modules" ]; then
     echo "/app/node_modules no existe, ejecutando npm install..."
@@ -19,6 +21,7 @@ npx prisma generate
 if [ "$DOCKER_ENVIRONMENT" = "dev" ]; then
     npm run start:dev
 else
+    npm install
     npm run start
 fi
 exit 0
